686ed11c0627a45646d8be26a5799f6e60e2e7fa
St-Jester <stellarjester@gmail.com>
2018-11-24 16:09:24 +0200

    minor fixes

---
+++ Assets/Editor/ShrinkBehaviourEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ShrinkBehaviour))]
+public class ShrinkBehaviourEditor : Editor
+{
+	override public void OnInspectorGUI()
+	{
+		var myScript = target as ShrinkBehaviour;
+
+		myScript.shrinkSpeed = EditorGUILayout.FloatField("Shrink Speed", myScript.shrinkSpeed);
+		myScript.targetScale = EditorGUILayout.FloatField("Target Scale", myScript.targetScale);
+
+		myScript.useRandom = GUILayout.Toggle(myScript.useRandom, "Use Random");
+
+		if (myScript.useRandom)
+		{
+			myScript.minRandom = EditorGUILayout.FloatField("Min Random", myScript.minRandom);
+			myScript.maxRandom = EditorGUILayout.FloatField("Max Random", myScript.maxRandom);
+		}
+	}
+}
--- Assets/Scripts/ShrinkBehaviour.cs
+++ Assets/Scripts/ShrinkBehaviour.cs
@@ -81,22 +81,4 @@ public class ShrinkBehaviour : MonoBehaviour {
 }
 
 
-[CustomEditor(typeof(ShrinkBehaviour))]
-public class ShrinkBehaviourEditor : Editor
-{
-	override public void OnInspectorGUI()
-	{
-		var myScript = target as ShrinkBehaviour;
-		
-		myScript.shrinkSpeed = EditorGUILayout.FloatField("Shrink Speed", myScript.shrinkSpeed);
-		myScript.targetScale = EditorGUILayout.FloatField("Target Scale", myScript.targetScale);
 
-		myScript.useRandom = GUILayout.Toggle(myScript.useRandom, "Use Random");
-
-		if (myScript.useRandom)
-		{
-			myScript.minRandom = EditorGUILayout.FloatField("Min Random", myScript.minRandom);
-			myScript.maxRandom = EditorGUILayout.FloatField("Max Random", myScript.maxRandom);
-		}
-	}
-}
